35d455fac9e5aab1144196df5d768ad1eb3d5c6f
javapark1 <dspark@netcrest.com>
2016-03-18 09:27:33 -0400

    Added equals() & hasCode() in CompanyKey.

--- pado-test/src/main/java/com/netcrest/pado/test/junit/CompanyKey.java
+++ pado-test/src/main/java/com/netcrest/pado/test/junit/CompanyKey.java
@@ -46,4 +46,33 @@ public class CompanyKey implements IRoutingKey, Serializable
 		return gics;
 	}
 
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + gics;
+		result = prime * result + ((symbol == null) ? 0 : symbol.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CompanyKey other = (CompanyKey) obj;
+		if (gics != other.gics)
+			return false;
+		if (symbol == null) {
+			if (other.symbol != null)
+				return false;
+		} else if (!symbol.equals(other.symbol))
+			return false;
+		return true;
+	}
 }
