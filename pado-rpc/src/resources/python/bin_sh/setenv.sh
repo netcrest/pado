efdd091dfb01499fd701bc516bdaa4a6123c81d3
javapark1 <dspark@netcrest.com>
2017-10-02 12:06:47 -0400

    Added support for pado-rpc-py packaging. It seems Python's PTYHONPATH has a bug. It does not recognize multiple paths. For now, unzip all packages in the lang/python/local-packages directory.

--- pado-rpc/src/resources/python/bin_sh/setenv.sh
+++ pado-rpc/src/resources/python/bin_sh/setenv.sh
@@ -13,4 +13,23 @@ if [ ! -d $DATA_DIR ]; then
   mkdir -p $DATA_DIR
 fi
 export PYTHON=python
-export PYTHONPATH=$BASE_DIR/src
+
+#
+# Application library path
+#
+# Append all jar files found in the $BASE_DIR/lib directory and
+# its subdirectories in the class path.
+#
+APP_ZIP=
+for file in `find $BASE_DIR/lib -name *.zip`
+do
+  if [ "${APP_ZIP}" ]; then
+    APP_ZIP=${APP_ZIP}:${file}
+  else
+    APP_ZIP=${file}
+  fi
+done
+
+# For some reason, PTYHONPATH does not work with multiple paths.
+# For now, unzip the entire library modules the local-packages directory.
+export PYTHONPATH=$BASE_DIR/local-packages:$APP_ZIP
